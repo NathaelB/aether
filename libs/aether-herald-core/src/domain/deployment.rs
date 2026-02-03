b3e15b3b802b398bfa97407f3b1053f389ce1aa7
ZUHOWKS <jojo.vilard38@gmail.com>
2026-02-03 22:28:36 +0100

    Add DeploymentId helpers and link to DataPlane
    Remove description field from Deployment and add dataplane_id of type
    DataPlaneId. Derive Hash for DeploymentId and implement new() and
    Display

--- libs/aether-herald-core/src/domain/deployment.rs
+++ libs/aether-herald-core/src/domain/deployment.rs
@@ -1,11 +1,25 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+use super::dataplane::DataPlaneId;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct DeploymentId(pub String);
+
+impl DeploymentId {
+    pub fn new(id: impl Into<String>) -> Self {
+        Self(id.into())
+    }
+}
+
+impl std::fmt::Display for DeploymentId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Deployment {
     pub id: DeploymentId,
+    pub dataplane_id: DataPlaneId,
     pub name: String,
-    pub description: String,
 }
